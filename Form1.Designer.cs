ca6db7465a8bed4e1edd7b2d1019e00dea93855e
Samaher25 <121268476+Samaher25@users.noreply.github.com>
2022-12-23 03:00:39 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(294, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sorting";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(291, 54);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(291, 80);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 2;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(291, 106);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 2;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(291, 132);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 20);
+            this.txt4.TabIndex = 3;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(291, 158);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 20);
+            this.txt5.TabIndex = 4;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(291, 184);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(100, 20);
+            this.txt6.TabIndex = 5;
+            this.txt6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // txt7
+            // 
+            this.txt7.Location = new System.Drawing.Point(291, 210);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(100, 20);
+            this.txt7.TabIndex = 6;
+            // 
+            // txt8
+            // 
+            this.txt8.Location = new System.Drawing.Point(291, 236);
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(100, 20);
+            this.txt8.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(38, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Insertion Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Results :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(128, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = " ";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(291, 394);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting
+{
+    public partial class Form1 : Form
+    {
+
+        public int num1;
+        public int num2;
+        public int num3;
+        public int num4;
+        public int num5;
+        public int num6;
+        public int num7;
+        public int num8;
+
+        int[] array = new int[8];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(txt1.Text);
+            num2 = int.Parse(txt2.Text);
+            num3 = int.Parse(txt3.Text);
+            num4 = int.Parse(txt4.Text);
+            num5 = int.Parse(txt5.Text);
+            num6 = int.Parse(txt6.Text);
+            num7 = int.Parse(txt7.Text);
+            num8 = int.Parse(txt8.Text);
+
+            array = new int[] { num1, num2, num3, num4, num5, num6, num7, num8 };
+
+
+            for ( int i = 1 ; i < array.Length ; i++ )
+            {
+                var key = array[i];
+                int j = i - 1;
+
+                while ( j >= 0 && array[j] > key )
+                {
+
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                    array[j + 1] = key;
+
+                }
+                    
+            }
+
+            string sorted_array = string.Join(",", array);
+            label3.Text = sorted_array;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+            txt3.Text = "";
+            txt4.Text = "";
+            txt5.Text = "";
+            txt6.Text = "";
+            txt7.Text = "";
+            txt8.Text = "";
+            label3.Text = "";
+        }
+    }
+}
